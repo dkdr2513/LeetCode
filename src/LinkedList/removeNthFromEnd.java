0512d9e965f4a6720e1c717a35bd35151295f039
dkdr2513 <50846877+dkdr2513@users.noreply.github.com>
2020-12-11 17:05:12 -0700

    12/11/2020

---
+++ src/LinkedList/removeNthFromEnd.java
@@ -0,0 +1,46 @@
+package LinkedList;
+
+public class removeNthFromEnd {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5, null)))));
+        _removeNthFromEnd(head, 2);
+        System.out.println(head.val);
+        System.out.println(head.next.val);
+        System.out.println(head.next.next.val);
+        System.out.println(head.next.next.next.val);
+    }
+
+    // Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+    public static ListNode _removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length  = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
---
+++ src/LinkedList/reverseLinkedList.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class reverseLinkedList {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5, null)))));
+        reverseList(head);
+        System.out.println(head.val);
+        System.out.println(head.next.val);
+        System.out.println(head.next.next.val);
+        System.out.println(head.next.next.next.val);
+        //System.out.println(head.next.next.next.next.val);
+    }
+
+    // reverse List
+    //
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+
+
